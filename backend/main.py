4be77277e8cb59e81b411ca364dd39b929cf8dd7
Matthew Hodorowski <matthewhods@gmail.com>
2026-07-23 22:13:52 -0400

    deploy backend to vercel

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from backend.routers.books import router as books_router
 from backend.routers.borrowers import router as borrowers_router
 from backend.routers.transactions import router as transactions_router
 
-app = FastAPI(title="Personal Library API")
+app = FastAPI(title="Morgan's Library API")
 
 app.include_router(books_router)
 app.include_router(borrowers_router)
@@ -12,4 +12,4 @@ app.include_router(transactions_router)
 
 @app.get("/")
 def root():
-    return {"message": "Personal Library API Running"}
+    return {"message": "Morgan's Library API Running"}
